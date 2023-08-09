dc1d4bb69bae48b69466b471da9d3f2f93939798
Florian Pichler <fp@ylk.gd>
2023-08-09 20:09:21 +0200

    Prefer `crypto` import over `node:crypto`
    Reduces compatibility issues

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -2,7 +2,8 @@ export async function getCrypto() {
   let crypto = globalThis.crypto;
 
   if (!crypto) {
-    crypto = await import('node:crypto').then((m) => m.webcrypto);
+    // eslint-disable-next-line unicorn/prefer-node-protocol
+    crypto = await import('crypto').then((m) => m.webcrypto);
   }
 
   return crypto;
